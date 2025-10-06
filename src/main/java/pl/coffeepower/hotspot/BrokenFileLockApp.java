16cc4ab3feb6989808942c9f3f67c2c3e3e1924e
Michal Jonko <michal.jonko@neotechnology.com>
2025-10-06 10:28:10 +0200

    cleaning code

--- src/main/java/pl/coffeepower/hotspot/BrokenFileLockApp.java
+++ src/main/java/pl/coffeepower/hotspot/BrokenFileLockApp.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
@@ -16,48 +17,56 @@ public final class BrokenFileLockApp {
 
     private static final Logger LOGGER = Logger.getLogger(BrokenFileLockApp.class.getName());
     private static final Path PATH = Paths.get(System.getProperty("java.io.tmpdir")).resolve("broken-file-lock.txt");
-    private static final Path JAR = Paths.get("/home/michaljonko/Java/Workspace/filelock-bug/build/libs/FileLock Bug-1.0-SNAPSHOT.jar");
+    private static final Path JAR = Paths.get(System.getProperty("user.dir")).resolve("build").resolve("libs").resolve("FileLock Bug-1.0-SNAPSHOT.jar");
 
     public static void main(String[] args) throws IOException, InterruptedException {
         if (args.length == 1) {
             LOGGER.info("Path: " + PATH);
             Files.deleteIfExists(PATH);
-            start(Boolean.parseBoolean(args[0]));
-        }
-        else if (args.length == 3) {
-            writer(Long.parseLong(args[0]), args[1], Boolean.parseBoolean(args[2]));
+            start(WriterTypes.valueOf(args[0].toUpperCase(Locale.ROOT)));
+        } else if (args.length == 3) {
+            write(Long.parseLong(args[0]), args[1], WriterTypes.valueOf(args[2].toUpperCase(Locale.ROOT)));
         } else {
             LOGGER.warning("NO PARAMS");
         }
     }
 
-    private static void writer(long sleepMs, String pattern, boolean buggy) throws IOException, InterruptedException {
+    private static void write(long sleepMs,
+                              String pattern,
+                              WriterTypes writerTypes) throws IOException, InterruptedException {
         final var pid = ProcessHandle.current().pid();
         LOGGER.info("Writer pid: " + pid);
 
         var content = new StringBuilder()
-                .repeat(pattern, 30)
+                .repeat(pattern, 10)
                 .append(System.lineSeparator())
                 .toString();
 
         try (var channel = FileChannel.open(PATH, Set.of(StandardOpenOption.CREATE, StandardOpenOption.APPEND));
-                var lock = channel.lock()) {
-            LOGGER.info("(" + pid + ") before substring(0,10)");
-            channel.write(ByteBuffer.wrap(content.substring(0, 10).getBytes()));
-            LOGGER.info("(" + pid + ") before substring(10,20)");
-            if(buggy) {
-                Files.writeString(PATH, content.substring(10, 20), StandardOpenOption.APPEND);
-            } else {
-                channel.write(ByteBuffer.wrap(content.substring(10, 20).getBytes()));
+             var lock = channel.lock()) {
+            channel.write(ByteBuffer.wrap(content.substring(0, content.length() / 3).getBytes()));
+            LOGGER.info("(" + pid + ") 1st write");
+
+            switch (writerTypes) {
+                case CHANNEL ->
+                        channel.write(ByteBuffer.wrap(content.substring(content.length() / 3, 2 * content.length() / 3).getBytes()));
+                case WRITE_STRING ->
+                        Files.writeString(PATH, content.substring(content.length() / 3, 2 * content.length() / 3), StandardOpenOption.APPEND);
+                case OUTPUT_STREAM -> {
+                    try (var outputStream = Files.newOutputStream(PATH, StandardOpenOption.APPEND)) {
+                        outputStream.write(content.substring(content.length() / 3, 2 * content.length() / 3).getBytes());
+                    }
+                }
             }
-            LOGGER.info("(" + pid + ") before sleep");
+
+            LOGGER.info("(" + pid + ") 2nd write");
             TimeUnit.MILLISECONDS.sleep(sleepMs);
-            LOGGER.info("(" + pid + ") before substring(20)");
-            channel.write(ByteBuffer.wrap(content.substring(20).getBytes()));
+            channel.write(ByteBuffer.wrap(content.substring(2 * content.length() / 3).getBytes()));
+            LOGGER.info("(" + pid + ") 3rd write");
         }
     }
 
-    private static void start(boolean buggy) throws IOException, InterruptedException {
+    private static void start(WriterTypes writerTypes) throws IOException, InterruptedException {
         var javaExec = Paths.get(System.getProperty("java.home")).resolve("bin").resolve("java");
         if (Files.notExists(javaExec)) {
             LOGGER.warning("No JAVA");
@@ -65,27 +74,11 @@ public final class BrokenFileLockApp {
         }
         LOGGER.info("Java: " + javaExec);
 
-        var processes = List.of(new ProcessBuilder(
-                        List.of(
-                                javaExec.toAbsolutePath().toString(),
-                                "-cp",
-                                JAR.toAbsolutePath().toString(),
-                                BrokenFileLockApp.class.getName(),
-                                "500",
-                                "A",
-                                Boolean.toString(buggy)))
+        var processes = List.of(new ProcessBuilder(createCommand(javaExec, "500", "BUG", writerTypes))
                         .inheritIO()
                         .redirectErrorStream(true)
                         .start(),
-                new ProcessBuilder(
-                        List.of(
-                                javaExec.toAbsolutePath().toString(),
-                                "-cp",
-                                JAR.toAbsolutePath().toString(),
-                                BrokenFileLockApp.class.getName(),
-                                "50",
-                                "a",
-                                Boolean.toString(buggy)))
+                new ProcessBuilder(createCommand(javaExec, "50", "buggy", writerTypes))
                         .inheritIO()
                         .redirectErrorStream(true)
                         .start());
@@ -95,5 +88,25 @@ public final class BrokenFileLockApp {
         }
         LOGGER.info("File content:");
         LOGGER.info(System.lineSeparator() + Files.readString(PATH));
+    }
+
+    private static List<String> createCommand(Path javaExec,
+                                              String number,
+                                              String BUG,
+                                              WriterTypes writerTypes) {
+        return List.of(
+                javaExec.toAbsolutePath().toString(),
+                "-cp",
+                JAR.toAbsolutePath().toString(),
+                BrokenFileLockApp.class.getName(),
+                number,
+                BUG,
+                writerTypes.toString());
+    }
+
+    private enum WriterTypes {
+        WRITE_STRING,
+        OUTPUT_STREAM,
+        CHANNEL;
     }
 }
